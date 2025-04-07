235e5f45d59e286092d45bc59daf57f14a4fe6bf
sailajasuprava <sailaja.suprava@gmail.com>
2025-04-07 20:04:30 +0530

    footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import FeedbackForm from "./components/FeedbackForm";
 import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <FeedbackForm />
+      <Footer />
       <Toaster />
     </div>
   );
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="mt-auto text-center text-gray-600 text-sm py-4">
+      Sailaja Suprava Mohanty | 2025
+    </footer>
+  );
+}
+
+export default Footer;
