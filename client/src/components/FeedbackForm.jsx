49927ecaee3f768b848e75a173183ae4bbac71f1
sailajasuprava <sailaja.suprava@gmail.com>
2025-04-07 19:30:27 +0530

    feedback form logic done

---
+++ client/src/components/FeedbackForm.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { FaSpinner } from "react-icons/fa6";
+import { toast } from "react-hot-toast";
+import axios from "../lib/axios";
+
+const initialState = {
+  fullname: "",
+  email: "",
+  message: "",
+};
+
+function FeedbackForm() {
+  const [formData, setFormData] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleFormData(e) {
+    setFormData((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  }
+
+  async function handleFormSubmission(e) {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const res = await axios.post("/feedbacks", formData);
+      toast.success(res.data.message);
+      setFormData(initialState);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <form className="w-full space-y-4" onSubmit={handleFormSubmission}>
+      <h1 className="text-3xl font-bold"> Create your account</h1>
+      <div>
+        <label htmlFor="fullname" className="label">
+          Full name
+        </label>
+        <input
+          type="text"
+          placeholder="John Doe"
+          id="fullname"
+          name="fullname"
+          required
+          className="input"
+          value={formData.fullname}
+          onChange={handleFormData}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email" className="label">
+          Email
+        </label>
+        <input
+          type="email"
+          placeholder="johndoe@gmail.com"
+          id="email"
+          name="email"
+          required
+          className="input"
+          value={formData.email}
+          onChange={handleFormData}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="message" className="label">
+          Password
+        </label>
+        <input
+          type="text"
+          placeholder="Enter your message"
+          id="message"
+          name="message"
+          required
+          className="input"
+          value={formData.message}
+          onChange={handleFormData}
+        />
+      </div>
+
+      <button type="submit" className="btn mt-2" disabled={isLoading}>
+        {isLoading ? (
+          <FaSpinner size={24} fill="white" className="animate-spin mx-auto" />
+        ) : (
+          "Create account"
+        )}
+      </button>
+    </form>
+  );
+}
+
+export default FeedbackForm;
--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const feedbackRouter = require("./routes/feedbackRoutes");
 const globalErrorHandler = require("./controllers/errorController");
+
+const corsOptions = {
+  origin: " http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors(corsOptions));
 app.use("/api/v1/feedbacks", feedbackRouter);
 
 app.all("*", (req, res, next) => {
